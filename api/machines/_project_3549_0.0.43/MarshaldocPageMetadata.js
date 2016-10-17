00dea3ee7164b69dbb3f521c21a969c53565608e
Rachael Shaw <r@rachael.wtf>
2016-10-17 18:22:25 -0500

    Revert "Update from master"
    This reverts commit 1aad2297bdcd015679bad7554f7b30ca0bd4dbdb, reversing
    changes made to ab9732a98dc634c0b7c134ec187454ca30d6de16.

--- api/machines/_project_3549_0.0.43/MarshaldocPageMetadata.js
+++ api/machines/_project_3549_0.0.43/MarshaldocPageMetadata.js
@@ -31,8 +31,7 @@ module.exports = {
         "displayNameSlug": "foo-bar",
         "parentDisplayName": "idk",
         "version": "0.3.6",
-        "isDeprecated": false,
-        "isMethod": false
+        "isDeprecated": false
       }]
     }
   },
@@ -65,13 +64,8 @@ module.exports = {
         var deprecatedChildPages = _.remove(childrenData, function(pageData) {
           return pageData.data.isDeprecated;
         });
-        // Grab out the method pages
-        var methodChildPages = _.remove(childrenData, function(pageData) {
-          return pageData.data.pageType === 'method';
-        });
-        // Now re-add the method pages at the end,
-        // followed by the deprecated pages.
-        childrenData = childrenData.concat(methodChildPages).concat(deprecatedChildPages);
+        // Then re-add them at the end.
+        childrenData = childrenData.concat(deprecatedChildPages);
 
         // Now put it back the way it was.
         // (As an array of `fullPathAndFileName`s)
@@ -161,8 +155,6 @@ module.exports = {
 
       docPage.isDeprecated = docPage.data.isDeprecated;
 
-      docPage.isMethod = docPage.data.isMethod;
-
       var pathSections = docPage.path.split('/');
       var parentPathIndex = pathSections.length - 2;
       docPage.parentDisplayName = pathSections[parentPathIndex];
@@ -188,13 +180,8 @@ module.exports = {
     var deprecatedPages = _.remove(inputs.docPageMetadatas, function(pageData) {
       return pageData.data.isDeprecated;
     });
-    // Grab out the method pages
-    var methodPages = _.remove(inputs.docPageMetadatas, function(pageData) {
-      return pageData.data.pageType === 'method';
-    });
-    // Then re-add the method pages at the end,
-    // followed by the deprecated pages.
-    inputs.docPageMetadatas = inputs.docPageMetadatas.concat(methodPages).concat(deprecatedPages);
+    // Then re-add them at the end.
+    inputs.docPageMetadatas = inputs.docPageMetadatas.concat(deprecatedPages);
 
 
     return exits.success(inputs.docPageMetadatas);
